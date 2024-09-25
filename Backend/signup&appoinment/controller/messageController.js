b3e48389bd1101d7ae4a63b79a3709358f82a641
Tanushka-lokegaonkar <tanushkaloke@gmail.com>
2024-09-25 17:08:44 +0530

    completed login

--- Backend/signup&appoinment/controller/messageController.js
+++ Backend/signup&appoinment/controller/messageController.js
@@ -1,19 +1,18 @@
+import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
 import { Message } from "../models/messageSchema.js";
+import ErrorHandler from "../middlewares/errorMiddleware.js";
 
-export const sendMessage = async (req, res, next) =>{
+export const sendMessage = catchAsyncErrors(async (req, res, next) =>{
     const { firstName, lastName, email, phone } = req.body;
     if (!firstName || !lastName || !email || !phone ) {
-      return res.status(400).json({
-        success: false,
-        message: "Please Fill Full Form",
-      });
+      return next(new ErrorHandler("Please Fill Full Form!", 400));
     }
     await Message.create({ firstName, lastName, email, phone });
     res.status(200).json({
       success: true,
       message: "Message Sent!",
     });
-};
+});
   
 //   export const getAllMessages = catchAsyncErrors(async (req, res, next) => {
 //     const messages = await Message.find();
